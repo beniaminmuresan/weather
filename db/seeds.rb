272e251e9b3f2e93263fd2d0b8ffdfcadc7b81e5
Muresan Beniamin <muresan.beniamin@hotmail.com>
2022-02-11 14:13:06 +0200

    Add WeatherPreference seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+temperature = 10
+%w(cold warm hot).each do |short_name|
+  weather_preference = WeatherPreference.find_or_initialize_by(short_name: short_name)
+  weather_preference.value = temperature
+  weather_preference.save
+  temperature += 10
+end
